caf5547d3a601a4ec9bb8e16eac5551a586ace66
Pedro Arrazola leon <44944743+arrazop@users.noreply.github.com>
2024-02-05 22:26:13 +0100

    💄 configured theme

---
+++ src/theme/colors.ts
@@ -0,0 +1,63 @@
+const colors = {
+  pureWhite: '#FFFFFF',
+  pureBlack: '#000',
+  main: '#F55679',
+  red: '#DC0B0B',
+  midGrey: '#808080',
+  primary: {
+    main: '#D90327',
+    dark: '#9E0000',
+    contrast: '#FFFFFF',
+    light: '#FB9492',
+    shade4: 'rgba(1, 1, 1, 0.04)',
+    shades8: 'rgba(1, 1, 1, 0.08)',
+    shade12: 'rgba(237, 0, 0, 0.12)',
+    shade30: 'rgba(217, 3, 39, 0.3)',
+    shade50: 'rgba(217, 3, 39, 0.5)',
+  },
+  secondary: {
+    main: '#1B85B5',
+    dark: '#005885',
+    light: '#5EB5E7',
+  },
+  text: {
+    primary: '#010101',
+    secondary: '#999999',
+    tertiary: '#E6E6E6',
+  },
+  grey: {
+    one: '#4A4747',
+    two: '#999898',
+    three: '#CBC6C6',
+    four: '#F2F0F1',
+  },
+  error: {
+    main: '#E96057',
+    dark: '#B12E2E',
+  },
+  outline: {
+    border: 'rgba(1, 1, 1, 0.23)',
+  },
+  success: {
+    main: '#47B588',
+    dark: '#008E43',
+    shade30: 'rgba(46, 192, 111, 0.3)',
+    shade8: 'rgba(46, 192, 111, 0.08)',
+  },
+  warning: {
+    main: '#E29F0E',
+    dark: '#C67F00',
+    shade8: 'rgba(255, 174, 3, 0.08)',
+  },
+  info: {
+    main: '#435BBE',
+    dark: '#00328D',
+  },
+  action: {
+    selected: 'rgba(1, 1, 1, 0.1)',
+    disabled: 'rgba(1, 1, 1, 0.24)',
+    active: 'rgba(1, 1, 1, 0.54)',
+  },
+};
+
+export default colors;
---
+++ src/theme/index.ts
@@ -0,0 +1,9 @@
+// my-theme.ts
+import {DefaultTheme} from 'styled-components';
+import colors from './colors';
+
+const myTheme: DefaultTheme = {
+  colors,
+};
+
+export {myTheme};
